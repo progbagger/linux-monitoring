7e63bb913b2dc3a39efbf18ee96defd44467c86b
Elene Mart <elenemar@student.21-school.ru>
2022-07-13 20:16:10 +0000

    develop: Changed quest 1 functions to work with quest 2

--- src/01/change_file_name.sh
+++ src/01/change_file_name.sh
@@ -5,18 +5,25 @@ function change_file_name() {
   local -n current_file_extension
   local available_names
   local available_extensions
+  local min_length
 
   current_file_name="$1"
   current_file_extension="$2"
   available_names="$3"
   available_extensions="$4"
 
+  if [[ -z $5 ]]; then
+    min_length=$5
+  else
+    min_length=4
+  fi
+
   # Изменяем имя файла
   if [[ "$current_file_name" == "" ]]; then
     for letter in $available_names; do
       current_file_name+="$letter"
     done
-    while [[ ${#current_file_name} -lt 4 ]]; do
+    while [[ ${#current_file_name} -lt $min_length ]]; do
       current_file_name+="${available_names: -1}"
     done
   else
--- src/01/change_folder_name.sh
+++ src/01/change_folder_name.sh
@@ -7,11 +7,18 @@ function change_folder_name() {
   local available_names
   available_names="$2"
 
+  local min_length
+  if [[ -z $3 ]]; then
+    min_length=$3
+  else
+    min_length=4
+  fi
+
   if [[ "$current_folder_name" == "" ]]; then
     for letter in $available_names; do
       current_folder_name+="$letter"
     done
-    while [[ ${#current_folder_name} -lt 4 ]]; do
+    while [[ ${#current_folder_name} -lt $min_length ]]; do
       current_folder_name+="${available_names: -1}"
     done
   else
