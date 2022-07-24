aed07312b75209a1ca4034c51c709f99bf3c68a3
Elene Mart <elenemar@student.21-school.ru>
2022-07-25 03:11:53 +0700

    develop: Quest 6 done?...

---
+++ src/06/main.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Путь к файлам лога
+path_to_logs="../04/nginx_logs"
+
+# Название файла конфигурации для GoAccess
+goaccess_conf="goaccess.conf"
+
+param=""
+
+result=0
+
+if [[ $# -eq 0 ]]; then
+
+  # Создаём строку файлов для конфа
+  files="$(ls "$path_to_logs")"
+  for file in $files; do
+    param+="$path_to_logs/$file "
+  done
+
+  goaccess -p "$goaccess_conf" $param
+else
+  result=1
+  echo "Script does not accepts any parameters."
+fi
+
+exit $result
