8d60921dd56dc611aff2aede2e7efd638c1ba915
Elene Mart <elenemar@student.21-school.ru>
2022-07-25 00:24:28 +0700

    develop: 4-th quest done!

---
+++ src/04/generate_record.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+
+function generate_ip() {
+  local ip=""
+
+  # Построение адреса
+  for ((i = 0; i < 4; i++)); do
+    ip+=$((RANDOM % 256))
+
+    # В адресе не должно быть точки после последнего числа
+    if [[ $i -ne $((4 - 1)) ]]; then
+      ip+="."
+    fi
+  done
+
+  echo "$ip"
+}
+
+function generate_response() {
+  # Создаём список допустимых кодов ответа
+  local response_list
+  response_list=(
+    200 # OK - запрос успешно обработан
+    201 # Created - ответ на PUT - ресурс создан
+    400 # Bad Request - неверный синтаксис запроса
+    401 # Unauthorized - для выполнения запроса требуется авторизация
+    403 # Forbidden - недостаточно прав для выполнения запроса
+    404 # Not Found - ресурс не найден
+    500 # Internal Server Error - сервер не обработал ситуацию
+    501 # Not Implemented - запрос не может быть обработан сервером
+    502 # Bad Gateway - получен недействительный ответ
+    503 # Service Unavailable - сервер не готов обработать запрос
+  )
+
+  # Выбираем случайный код из списка
+  local num
+  num=$((RANDOM % ${#response_list[@]}))
+
+  echo "${response_list[$num]}"
+}
+
+function generate_method() {
+  # Создаём список допустимых методов
+  local methods_list
+  methods_list=("GET" "POST" "PUT" "PATCH" "DELETE")
+
+  # Выбираем случайный метод из списка
+  local num
+  num=$((RANDOM % ${#methods_list[@]}))
+
+  echo "${methods_list[$num]}"
+}
+
+function generate_agent() {
+  # Создаём список допустимых агентов
+  local agents_list
+  agents_list=("Mozilla" "Google Chrome" "Opera" "Safari" "Internet Explorer" "Microsoft Edge" "Crawler and bot" "Library and net tool")
+
+  # Выбираем случайный агент из списка
+  local num
+  num=$((RANDOM % ${#agents_list[@]}))
+
+  echo "${agents_list[$num]}"
+}
+
+function generate_url() {
+  # Создаём список URL-ов
+  local url_list
+  url_list=(
+    "https://google.ru"
+    "https://yandex.ru"
+    "https://edu.21-school.ru"
+    "https://youtube.com"
+    "https://vk.com"
+    "https://stepik.org"
+    "https://habr.com"
+    "https://vc.ru"
+  )
+
+  # Выбираем случайный URL из списка
+  local num
+  num=$((RANDOM % ${#url_list[@]}))
+
+  echo "${url_list[$num]}"
+}
+
+# $1 - предыдущая дата/время; если задана только дата - создать время с нуля,
+#      иначе - увеличить время
+function generate_date() {
+  local date
+  date="$1"
+
+  if [[ "$date" =~ ^[0-9][0-9]-[0-9][0-9]-[0-9][0-9]$ ]]; then
+    date="$date"" 00:00:00"
+  else
+
+    # Парсим день-месяц-год
+    local ymd
+    ymd="$(grep -Eo '^.* ' <<<"$date" | sed 's/ //')"
+
+    # Парсим час-минуты-секунды
+    local hms
+    hms="$(grep -Eo ' .*$' <<<"$date" | sed 's/ //')"
+
+    # Вытаскиваем часы
+    local hours
+    hours="$(grep -Eo '^[0-9][0-9]' <<<"$hms")"
+    if [[ "${hours:0:1}" == "0" && "${hours:1:1}" != "0" ]]; then
+      hours="${hours: -1}"
+    fi
+
+    # Вытаскимаем минуты
+    local minutes
+    minutes="$(grep -Eo ':[0-9][0-9]:' <<<"$hms" | sed 's/://; s/://')"
+    if [[ "${minutes:0:1}" == "0" && "${minutes:1:1}" != "0" ]]; then
+      minutes="${minutes: -1}"
+    fi
+
+    # Выбираем случайные секунды
+    local seconds
+    seconds=$((RANDOM % 60))
+
+    # Увеличиваем минуты на 1
+    minutes=$((minutes + 1))
+
+    # Если минут больше 59, то сбросить их на 0 и увеличить часы
+    if [[ $minutes -ge 60 ]]; then
+      minutes=0
+      ((hours++))
+    fi
+
+    # Формируем итоговую дату
+    date="$ymd "
+    if [[ ${#hours} -eq 1 ]]; then
+      date+="0"
+    fi
+    date+="$hours:"
+
+    if [[ ${#minutes} -eq 1 ]]; then
+      date+="0"
+    fi
+    date+="$minutes:"
+
+    if [[ ${#seconds} -eq 1 ]]; then
+      date+="0"
+    fi
+    date+="$seconds"
+  fi
+
+  echo "$date"
+}
+
+# $1 - текущая дата (+ время). Если без времени - собирается дата по умолчанию: <дата> 00:00:00
+function generate_record() {
+  local current_date
+  current_date="$1"
+
+  local record=""
+
+  # Добавляем IP
+  record+="$(generate_ip)"" "
+
+  # Добавляем код ответа
+  record+="$(generate_response)"" "
+
+  # Добавляем метод
+  record+="$(generate_method)"" "
+
+  # Добавляем дату
+  record+="$(generate_date "$current_date")"" "
+
+  # Добавляем URL
+  record+="$(generate_url)"" "
+
+  # Добавляем агент
+  record+="$(generate_agent)"
+
+  echo "$record"
+}
--- src/04/main.sh
+++ src/04/main.sh
@@ -2,4 +2,48 @@
 # Отключаем проверку подключаемых файлов
 # shellcheck disable=SC1091
 
+source ./generate_record.sh
 
+# Создаём цвета
+reset="\033[m"
+green="\033[92m"
+red="\033[91m"
+purple="\033[95m"
+
+# Создаем папку для логов
+mkdir "nginx_logs"
+cd "nginx_logs" || exit 1
+
+# Стартовая дата
+start_date="20-07-22"
+
+total_files=0
+
+# Генерируем пять дней
+for ((logname = 1; logname <= 5; logname++)); do
+
+  # Выводим сообщение о создании очередного файла логов
+  echo -en "$red""Generating file ""$reset""$purple""$logname.log""$reset""$red"" ...""$reset"
+  curdate="$start_date"
+
+  # Считаем количество создаваемых записей в логе
+  num=$((RANDOM % 901 + 100))
+
+  # Считаем общее количество записей
+  total_files=$((total_files + num))
+
+  # Генерируем запись
+  for ((i = 0; i < num; i++)); do
+    curdate="$(generate_date "$curdate")"
+    generate_record "$curdate" >>"$logname.log"
+  done
+
+  # Выводим сообщение об окончании генерации файла лога
+  echo -e "\b\b\b""$green""Done. Generated ""$reset""$purple""$num""$reset""$green"" records.""$reset"
+
+  # Меняем дату
+  start_date="${start_date:0:1}""$((${start_date:1:1} + 1))""${start_date:2}"
+done
+
+# Выводим итоговую инфу
+echo -e "$green""Script has done its work. Total ""$reset""$purple""$total_files""$reset""$green"" records generated."
